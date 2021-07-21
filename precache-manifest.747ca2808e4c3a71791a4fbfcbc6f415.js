111d181ccfda7a4414596f58916289a0ede08bd9
rasandilikshana <rasandilikshana@gmail.com>
2021-07-21 15:35:40 +0530

    Updates

---
+++ precache-manifest.747ca2808e4c3a71791a4fbfcbc6f415.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1d0aa5d42ba6e65919adaab6ece6e2",
+    "url": "/CourseDiscription/index.html"
+  },
+  {
+    "revision": "230a8321c6983e44e994",
+    "url": "/CourseDiscription/static/js/2.750f14cf.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/CourseDiscription/static/js/2.750f14cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0593643d70b7e988e83e",
+    "url": "/CourseDiscription/static/js/main.777e5574.chunk.js"
+  },
+  {
+    "revision": "40cbc1c4baaefef863e1",
+    "url": "/CourseDiscription/static/js/runtime-main.832062d4.js"
+  },
+  {
+    "revision": "ccb2cad24e4499edf0d2e61de290b60f",
+    "url": "/CourseDiscription/static/media/What.ccb2cad2.jpg"
+  },
+  {
+    "revision": "63304d4e37af310c54e3c6009aed730b",
+    "url": "/CourseDiscription/static/media/profile.63304d4e.png"
+  },
+  {
+    "revision": "5ca853a238afd26c85fccb1c02fdcd31",
+    "url": "/CourseDiscription/static/media/web.5ca853a2.svg"
+  },
+  {
+    "revision": "3b591683222136e9e9139c687b0beb00",
+    "url": "/CourseDiscription/static/media/what2.3b591683.png"
+  }
+]);
